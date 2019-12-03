df8248d4f3f26b35593d0ac2ca3e573b3c4a410d
kadey001 <kadey001@ucr.edu>
2019-12-02 19:40:23 -0800

    Fixed Render Problems and Implemented other types

--- project2/driver_state.cpp
+++ project2/driver_state.cpp
@@ -39,7 +39,7 @@ void render(driver_state& state, render_type type)
 {
     switch(type) {
         case render_type::triangle: {
-            data_geometry* triangle_geometry = new data_geometry[3];
+            const data_geometry* triangle_geometry[3];
             data_geometry geometry_d[3];
             data_vertex vertex_d[3];
             //If the render is a triangle, the number of triangles should be
@@ -53,28 +53,76 @@ void render(driver_state& state, render_type type)
             for(size_t triangle = 0; triangle < state.num_triangles; triangle++) {
                 for(size_t i = 0; i < 3; i++) {
                     //std::cout << *(state.vertex_data + index) << std::endl;
-                    triangle_geometry[i].data = state.vertex_data + index;
-                    vertex_d[i].data = triangle_geometry[i].data;
+                    vertex_d[i].data = state.vertex_data + index;
+                    geometry_d[i].data = vertex_d[i].data;
                     //std::cout << *triangle_geometry[i].data << std::endl;
 
                     //Fill gl_Position for the triangle geometry allowing it to be accessed while clipping
-                    state.vertex_shader(vertex_d[i], triangle_geometry[i], state.uniform_data);
+                    state.vertex_shader(vertex_d[i], geometry_d[i], state.uniform_data);
+
+                    triangle_geometry[i] = &geometry_d[i];
                     //std::cout << triangle_geometry[i].gl_Position << std::endl;
 
                     index += state.floats_per_vertex;
                 }
                 //rasterize_triangle(state, (const data_geometry**)&triangle_geometry);
-                clip_triangle(state, (const data_geometry**)&triangle_geometry, 0);
+                clip_triangle(state, triangle_geometry, 0);
             }
             break;
         }
         case render_type::indexed: {
+            const data_geometry* triangle_geometry[3];
+            data_geometry geometry_d[3];
+            data_vertex vertex_d[3];
+
+            for(size_t index = 0; index < state.num_triangles * 3; index += 3) {
+                for(size_t i = 0; i < 3; i++) {
+                    vertex_d[i].data = &state.vertex_data[state.index_data[index + i] * state.floats_per_vertex];
+                    geometry_d[i].data = vertex_d[i].data;
+                    state.vertex_shader(vertex_d[i], geometry_d[i], state.uniform_data);
+                    triangle_geometry[i] = &geometry_d[i];
+                }
+                clip_triangle(state, triangle_geometry, 0);
+            }
             break;
         }
         case render_type::fan: {
+            const data_geometry* triangle_geometry[3];
+            data_geometry geometry_d[3];
+            data_vertex vertex_d[3];
+            int temp = 0;
+
+            for(size_t fan = 0; fan < state.num_vertices; fan++) {
+                for(size_t i = 0; i < 3; i++) {
+                    if(i == 0) {
+                        temp = 0;
+                    } else {
+                        temp = fan + i;
+                    }
+
+                    vertex_d[i].data = &state.vertex_data[temp * state.floats_per_vertex];
+                    geometry_d[i].data = vertex_d[i].data;
+                    state.vertex_shader(vertex_d[i], geometry_d[i], state.uniform_data);
+                    triangle_geometry[i] = &geometry_d[i];
+                }
+                clip_triangle(state, triangle_geometry, 0);
+            }
             break;
         }
         case render_type::strip: {
+            const data_geometry* triangle_geometry[3];
+            data_geometry geometry_d[3];
+            data_vertex vertex_d[3];
+
+            for(size_t strip = 0; strip < state.num_vertices - 2; strip++) {
+                for(size_t i = 0; i < 3; i++) {
+                    vertex_d[i].data = &state.vertex_data[(strip + i) * state.floats_per_vertex];
+                    geometry_d[i].data = vertex_d[i].data;
+                    state.vertex_shader(vertex_d[i], geometry_d[i], state.uniform_data);
+                    triangle_geometry[i] = &geometry_d[i];
+                }
+                clip_triangle(state, triangle_geometry, 0);
+            }
             break;
         }
         default: {
@@ -88,14 +136,20 @@ void render(driver_state& state, render_type type)
 // It will be called recursively, once for each clipping face (face=0, 1, ..., 5) to
 // clip against each of the clipping faces in turn.  When face=6, clip_triangle should
 // simply pass the call on to rasterize_triangle.
-void clip_triangle(driver_state& state, const data_geometry* in[3],int face)
+void clip_triangle(driver_state& state, const data_geometry* in[3], int face)
 {
     //TODO Finish triangle clipping
     data_geometry geometry_d[3] = {(*in)[0], (*in)[1], (*in)[2]};
     vec4 A = geometry_d[0].gl_Position;
     vec4 B = geometry_d[1].gl_Position;
     vec4 C = geometry_d[2].gl_Position;
+    double lerp_factor_1, lerp_factor_2;
     enum {x, y, z, w};
+
+    data_geometry D1[3], D2[3];
+
+    float A1, B1, B2;
+    vec4 P1, P2;
     
     switch (face)
     {
@@ -104,49 +158,169 @@ void clip_triangle(driver_state& state, const data_geometry* in[3],int face)
         // std::cout << A << std::endl;
         // std::cout << B << std::endl;
         // std::cout << C << std::endl;
-        if(A[x] > -1 && B[x] > -1 && C[x] > -1) {
+        if(A[x] >= -A[w] && B[x] >= -B[w] && C[x] >= -C[w]) {
             //No clipping needed for left face
             break;
+        } else if (A[x] < -A[w] && B[x] < -B[w] && C[x] < -C[w])  {
+            //Return since triangle is not inside at all, no need to rasterize
+            return;
         } else {
-            //Need to clip the left face
+            //Figure out which points are outside
+            if(A[x] < -A[w] && B[x] >= -B[w] && C[x] >= -C[w]) {
+                //Only A outside
+                lerp_factor_1 = (-B[w] - B[x]) / (A[x] + A[w] - B[w] - B[x]);
+                lerp_factor_2 = (-C[w] - C[x]) / (A[x] + A[w] - C[w] - C[x]);
 
+                //vec4 new_p0 = lerp_factor_1 * A + (1 - lerp_factor_1) * B;
+                //vec4 new_p1 = lerp_factor_2 * A + (1 - lerp_factor_2) * C;
+                // vec4 Compute_Intersection(const float &beta, const vec4 &start_vertex, const vec4 &end_vertex)
+                // {
+                //     return beta * start_vertex + (1 - beta) * end_vertex;
+                // }
+
+                // vec4 new_p0 = Compute_Intersection(edges[0].beta, edges[0].start_vertex, edges[0].end_vertex);
+                // vec4 new_p1 = Compute_Intersection(edges[1].beta, edges[1].start_vertex, edges[1].end_vertex);
+                // data_geometry new_vertices0[3];
+                // data_geometry new_vertices1[3];
+                // vector<vector<data_geometry>> new_triangles;
+
+                // new_vertices0[0].data = new float[state.floats_per_vertex];
+                // Interpolate(state, edges[1], new_vertices0[0], 0, in);
+                // new_vertices0[0].gl_Position = new_p1;
+                // vector<data_geometry> new_tri0 = {new_vertices0[0], *in[1], *in[2]};
+                // new_triangles.emplace_back(new_tri0);     
+
+                // new_vertices1[0].data = new float[state.floats_per_vertex];
+                // Interpolate(state, edges[0], new_vertices1[0], 0, in);
+                // new_vertices1[0].gl_Position = new_p0;
+                // vector<data_geometry> new_tri1 = {new_vertices1[0], *in[1], new_vertices0[0]};
+                // new_triangles.emplace_back(new_tri1); 
+                //std::cout << lerp_factor_1 << std::endl;
+            } else if(A[x] >= -A[w] && B[x] < -B[w] && C[x] >= -C[w]) {
+                //Only B outside
+                lerp_factor_1 = (-B[w] - B[x]) / (A[x] + A[w] - B[w] - B[x]);
+            } else if(A[x] >= -A[w] && B[x] >= -B[w] && C[x] < -C[w]) {
+                //Only C outside
+                lerp_factor_1 = (-B[w] - B[x]) / (A[x] + A[w] - B[w] - B[x]);
+            } else if(A[x] < -A[w] && B[x] < -B[w] && C[x] >= -C[w]) {
+                //A and B outside
+                lerp_factor_1 = (-B[w] - B[x]) / (A[x] + A[w] - B[w] - B[x]);
+            } else if(A[x] < -A[w] && B[x] >= -B[w] && C[x] < -C[w]) {
+                //A and C outside
+                lerp_factor_1 = (-B[w] - B[x]) / (A[x] + A[w] - B[w] - B[x]);
+            } else if(A[x] >= -A[w] && B[x] < -B[w] && C[x] < -C[w]) {
+                //B and C outside
+                lerp_factor_1 = (-B[w] - B[x]) / (A[x] + A[w] - B[w] - B[x]);
+            }
         }
         break;
     case 1:
         //Right
-        if(A[x] < 1 && B[x] < 1 && C[x] < 1) {
+        if(A[x] <= A[w] && B[x] <= B[w] && C[x] <= C[w]) {
             //No clipping needed for right face
             break;
+        } else if(A[x] > A[w] && B[x] > B[w] && C[x] > C[w]) {
+            //Return since triangle is not inside at all, no need to rasterize
+            return;
         } else {
-            //Need to clip the right face
-            
+            //Figure out which points are outside
+            if(A[x] <= A[w] && B[x] > B[w] && C[x] > C[w]) {
+                //Only A outside
+                lerp_factor_1 = (B[w] - B[x]) / (A[x] + A[w] + B[w] - B[x]);
+                //std::cout << lerp_factor_1 << std::endl;
+            } else if(A[x] <= A[w] && B[x] > B[w] && C[x] <= C[w]) {
+                //Only B outside
+            } else if(A[x] <= A[w] && B[x] <= B[w] && C[x] > C[w]) {
+                //Only C outside
+            } else if(A[x] > A[w] && B[x] > B[w] && C[x] <= C[w]) {
+                //A and B outside
+            } else if(A[x] > A[w] && B[x] <= B[w] && C[x] > C[w]) {
+                //A and C outside
+            } else if(A[x] <= A[w] && B[x] > B[w] && C[x] > C[w]) {
+                //B and C outside
+            }
         }
         break;
     case 2:
         //Top
-        if(A[y] < 1 && B[y] < 1 && C[y] < 1) {
+        if(A[y] <= A[w] && B[y] <= B[w] && C[y] <= C[w]) {
             //No clipping needed for top face
             break;
+        } else if(A[y] > A[w] && B[y] > B[w] && C[y] > C[w]) {
+            //Return since triangle is not inside at all, no need to rasterize
+            return;
         } else {
-            //Need to clip the top face
-            
+            //Figure out which points are outside
+            if(A[y] <= A[w] && B[y] > B[w] && C[y] > C[w]) {
+                //Only A outside
+                lerp_factor_1 = (B[w] - B[y]) / (A[y] + A[w] + B[w] - B[y]);
+                //std::cout << lerp_factor_1 << std::endl;
+            } else if(A[y] <= A[w] && B[y] > B[w] && C[y] <= C[w]) {
+                //Only B outside
+            } else if(A[y] <= A[w] && B[y] <= B[w] && C[y] > C[w]) {
+                //Only C outside
+            } else if(A[y] > A[w] && B[y] > B[w] && C[y] <= C[w]) {
+                //A and B outside
+            } else if(A[y] > A[w] && B[y] <= B[w] && C[y] > C[w]) {
+                //A and C outside
+            } else if(A[y] <= A[w] && B[y] > B[w] && C[y] > C[w]) {
+                //B and C outside
+            }
         }
         break;
     case 3:
         //Bottom
-        if(A[y] > -1 && B[y] > -1 && C[y] > -1) {
+        if(A[y] >= -A[w] && B[y] >= -B[w] && C[y] >= -C[w]) {
             //No clipping needed for bottom face
             break;
+        } else if (A[x] < -A[w] && B[x] < -B[w] && C[x] < -C[w])  {
+            //Return since triangle is not inside at all, no need to rasterize
+            return;
         } else {
-            //Need to clip the bottom face
-            
+            //Figure out which points are outside
+            if(A[y] < -A[w] && B[y] >= -B[w] && C[y] >= -C[w]) {
+                //Only A outside
+                lerp_factor_1 = (-B[w] - B[y]) / (A[y] + A[w] - B[w] - B[y]);
+                //std::cout << lerp_factor_1 << std::endl;
+            } else if(A[y] >= -A[w] && B[y] < -B[w] && C[y] >= -C[w]) {
+                //Only B outside
+            } else if(A[y] >= -A[w] && B[y] >= -B[w] && C[y] < -C[w]) {
+                //Only C outside
+            } else if(A[y] < -A[w] && B[y] < -B[w] && C[y] >= -C[w]) {
+                //A and B outside
+            } else if(A[y] < -A[w] && B[y] >= -B[w] && C[y] < -C[w]) {
+                //A and C outside
+            } else if(A[y] >= -A[w] && B[y] < -B[w] && C[y] < -C[w]) {
+                //B and C outside
+            }
         }
         break;
     case 4:
         //Front
+        if(A[z] <= A[w] && B[z] <= B[w] && C[y] <= C[w]) {
+            break;
+        } else if(A[z] > A[w] && B[z] > B[w] && C[z] > C[w]) {
+            return;
+        } else {
+
+        }
         break;
     case 5:
         //Back
+        if(A[z] >= -A[w] && B[z] >= -B[w] && C[y] >= -C[w]) {
+            break;
+        } else if(A[z] < -A[w] && B[z] < -B[w] && C[z] < -C[w]) {
+            return;
+        } else {
+            // if(A[z] < -A[w] && B[z] >= -B[w] && C[z] >= -C[w]) {
+                
+            // }
+            // D1[x].data = new float[state.floats_per_vertex];
+            // D1[y] = geometry_d[y];
+            // D1[z] = geometry_d[z];
+
+
+        }
         break;
     default:
         rasterize_triangle(state, in);
@@ -260,7 +434,7 @@ void rasterize_triangle(driver_state& state, const data_geometry* in[3])
             gamma = (.5 * (part3 + (pixel_y[a] - pixel_y[b]) * i + (pixel_x[b] - pixel_x[a]) * j) / triangle_area); 
 
             //Find depth for Z-Buffering
-            float depth = (alpha * z[a]) + (beta * z[b]) + (gamma * z[c]);
+            float depth = alpha * z[a] + beta * z[b] + gamma * z[c];
 
             //Only render pixel when the point is inside of the triangle and 
             if(alpha > 0 && beta > 0 && gamma > 0 && state.image_depth[i + j * w] > depth) {
@@ -270,6 +444,10 @@ void rasterize_triangle(driver_state& state, const data_geometry* in[3])
                 
                 //Interpolation
                 for (int k = 0; k < state.floats_per_vertex; k++) {
+                    const float ALPHA = alpha;
+                    const float BETA = beta;
+                    const float GAMMA = gamma;
+                    float temp;
                     switch (state.interp_rules[k]) {
                         case interp_type::flat:
                             //Flat fragment shader
@@ -284,6 +462,20 @@ void rasterize_triangle(driver_state& state, const data_geometry* in[3])
                                 gamma * geometry_d[c].data[k]
                             );
                             //std::cout << fragment_d.data[k] << std::endl;
+                            break;
+
+                        case interp_type::smooth:
+                            temp = (ALPHA / w0) + (BETA / w1) + (GAMMA / w2);
+                            
+                            alpha = ALPHA / temp / w0;
+                            beta = BETA / temp / w1;
+                            gamma = GAMMA / temp / w2;
+
+                            fragment_d.data[k] = (
+                                alpha * geometry_d[a].data[k] + 
+                                beta * geometry_d[b].data[k] + 
+                                gamma * geometry_d[c].data[k]
+                            );
                             break;
 
                         default:
